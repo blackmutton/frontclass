da5a80aa517eb2183a86f3287debd3af4d38c3d4
yt Chen <yutongc30@gmail.com>
2024-08-19 09:51:37 +0800

    補充cors利用header開放其他網站連線的方法

--- s20240819/data.php
+++ s20240819/data.php
@@ -1,4 +1,13 @@
 <?php
+// 'Access-Control-Allow-Origin: *'代表開放全部網址皆可使用，如果是'Access-Control-Allow-Origin: http://localhost/frontend/s20240819/s20240819_01_cors.html'則只有指定網址可以連線這個api
+header('Access-Control-Allow-Origin: *');
+// 可以指定多個開放網址
+// header('Access-Control-Allow-Origin: http://localhost/frontend/s20240819/s20240819_01_cors.html');
+// header('Access-Control-Allow-Origin: http://apple.com');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 function dd($array)
 {
     echo "<pre>";
